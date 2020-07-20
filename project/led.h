8a83d760cc22d818dc9ee6584e93c83661b32de5
Felipe Leal <felipe.leal.sierra@gmail.com>
2020-07-19 18:23:19 -0600

    troubleshooting toggle_led not being picked up

--- project/led.h
+++ project/led.h
@@ -1,5 +1,6 @@
 #ifndef led_included
 #define led_included
+#include <msp430.h>
 
 #define LED_RED BIT0               // P1.0
 #define LED_GREEN BIT6             // P1.6
@@ -8,6 +9,6 @@
 void led_init();
 void led_update();
 
-extern unsigned char leds_changed, green_led_state, red_led_state, led_off;
+extern unsigned char toggle_led;
 
 #endif // included
