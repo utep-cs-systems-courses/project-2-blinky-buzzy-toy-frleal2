36e7b07980c88ad6178866adf1b284093923e8fc
Felipe Leal <felipe.leal.sierra@gmail.com>
2020-07-21 22:24:46 -0600

    leds

--- project/led.c
+++ project/led.c
@@ -12,12 +12,12 @@ static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
 void led_init()
 {
   P1DIR |= LEDS;		// bits attached to leds are output
-  char ledFlags = redVal[red_on] | greenVal[green_on];
+  //char ledFlags = redVal[red_on] | greenVal[green_on];
   led_update();
 }
 
 void led_update(){
-  char ledFlags;
+  char ledFlags =0;
 
   if (switch_state == 1){
     char ledFlags = redVal[red_on] | greenVal[green_on];
