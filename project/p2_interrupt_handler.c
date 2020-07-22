234f130b56f0e0d0cec74c3b73e27e810efa0c8c
Felipe Leal <felipe.leal.sierra@gmail.com>
2020-07-21 23:07:49 -0600

    p2 handler

--- project/p2_interrupt_handler.c
+++ project/p2_interrupt_handler.c
@@ -4,7 +4,7 @@
 /* Switch on P2 (S1,S2,S3,S4) */
 void
 __interrupt_vec(PORT1_VECTOR) Port_2(){
-  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+  if (P2IFG & SWITCHES) {	      
     P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
     switch_interrupt_handler();	/* single handler for all switches */
   }
